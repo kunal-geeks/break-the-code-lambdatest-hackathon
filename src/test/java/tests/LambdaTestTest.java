fac7be89f99f27f31672a4a4216bf45c3983ddc1
Kunal Sharma <kunal.ucet@gmail.com>
2025-04-12 20:42:26 +0530

    Refactor LambdaTestTest to inherit from BaseTest for driver setup

--- src/test/java/tests/LambdaTestTest.java
+++ src/test/java/tests/LambdaTestTest.java
@@ -1,5 +1,25 @@
 package tests;
 
-public class LambdaTestTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class LambdaTestTest extends BaseTest {
+
+    @Test
+    public void testLambdaTestWebsite() {
+        // Navigate to the LambdaTest website
+        driver.get("https://www.lambdatest.com");
+
+        // Assert that the LambdaTest title is present
+        Assert.assertTrue(driver.getTitle().contains("LambdaTest"));
+    }
+
+    @Test
+    public void testHomePageTitle() {
+        // Navigate to a sample website
+        driver.get("https://www.google.com");
+
+        // Assert that the Google title is correct
+        Assert.assertEquals(driver.getTitle(), "Google");
+    }
 }
