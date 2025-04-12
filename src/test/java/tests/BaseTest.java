3f3dfeedef1179f146735c78b8d743f1ab2a1da6
Kunal Sharma <kunal.ucet@gmail.com>
2025-04-12 20:28:23 +0530

    Refactor BaseTest to integrate headless parameter and centralized setup

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,5 +1,25 @@
 package tests;
 
-public class BaseTest {
+import driver.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    @Parameters({"browser", "version", "platform", "headless"})
+    public void setUp(String browser, String version, String platform, boolean headless) {
+        driver = DriverFactory.getDriver(browser, version, platform, headless);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println("Driver quit...");
+        }
+    }
 }
