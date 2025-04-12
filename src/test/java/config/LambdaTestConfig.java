9302220ade815343451dcee7ae331ab64e065d46
Kunal Sharma <kunal.ucet@gmail.com>
2025-04-12 19:18:07 +0530

    feat: add LambdaTestConfig for remote capabilities setup

---
+++ src/test/java/config/LambdaTestConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class LambdaTestConfig {
+    public static final String LT_USERNAME = System.getenv("LT_USERNAME");
+    public static final String LT_ACCESS_KEY = System.getenv("LT_ACCESS_KEY");
+
+    public static final String GRID_URL = "https://" + LT_USERNAME + ":" + LT_ACCESS_KEY + "@hub.lambdatest.com/wd/hub";
+
+    public static DesiredCapabilities getCapabilities(String browser, String version, String platform) {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("browserName", browser);
+        caps.setCapability("version", version);
+        caps.setCapability("platform", platform);
+        caps.setCapability("build", "LambdaTest Hackathon Build");
+        caps.setCapability("name", "LambdaTest Automation Test");
+        caps.setCapability("network", true);
+        caps.setCapability("visual", true);
+        caps.setCapability("video", true);
+        caps.setCapability("console", true);
+        return caps;
+    }
+}
