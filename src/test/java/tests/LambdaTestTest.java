7bebb3d4fb2490e6d8a6ccf1f3e538d05e176d2e
Kunal Sharma <kunal.ucet@gmail.com>
2025-04-12 22:02:53 +0530

    refactor: Replaced System.out with Logger in LambdaTestTest for improved logging

--- src/test/java/tests/LambdaTestTest.java
+++ src/test/java/tests/LambdaTestTest.java
@@ -1,25 +1,58 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.apache.logging.log4j.Logger;
+import utils.LoggerUtil;
+
+
 
 public class LambdaTestTest extends BaseTest {
 
-    @Test
-    public void testLambdaTestWebsite() {
-        // Navigate to the LambdaTest website
-        driver.get("https://www.lambdatest.com");
-
-        // Assert that the LambdaTest title is present
-        Assert.assertTrue(driver.getTitle().contains("LambdaTest"));
-    }
+    private static final Logger logger = LoggerUtil.getLogger(LambdaTestTest.class);
 
     @Test
-    public void testHomePageTitle() {
-        // Navigate to a sample website
-        driver.get("https://www.google.com");
+    public void verifyTodoListFunctionality() throws InterruptedException {
+        String spanText;
 
-        // Assert that the Google title is correct
-        Assert.assertEquals(driver.getTitle(), "Google");
+        logger.info("Opening LambdaTest Todo App");
+        driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+        // Interact with existing todo list items
+        logger.info("Clicking existing list items");
+        driver.findElement(By.name("li1")).click();
+        driver.findElement(By.name("li2")).click();
+        driver.findElement(By.name("li3")).click();
+        driver.findElement(By.name("li4")).click();
+
+        // Add new items
+        logger.info("Adding new list items");
+        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 6");
+        driver.findElement(By.id("addbutton")).click();
+
+        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 7");
+        driver.findElement(By.id("addbutton")).click();
+
+        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 8");
+        driver.findElement(By.id("addbutton")).click();
+
+        // Interact with more items
+        logger.info("Interacting with added list items");
+        driver.findElement(By.name("li1")).click();
+        driver.findElement(By.name("li3")).click();
+        driver.findElement(By.name("li7")).click();
+        driver.findElement(By.name("li8")).click();
+
+        // Final add and assert
+        logger.info("Adding final list item and asserting its presence");
+        driver.findElement(By.id("sampletodotext")).sendKeys("Get Taste of Lambda and Stick to It");
+        driver.findElement(By.id("addbutton")).click();
+        driver.findElement(By.name("li9")).click();
+
+        spanText = driver.findElement(By.xpath("/html/body/div/div/div/ul/li[9]/span")).getText();
+        Assert.assertEquals(spanText, "Get Taste of Lambda and Stick to It");
+
+        logger.info("Test completed successfully.");
     }
 }
