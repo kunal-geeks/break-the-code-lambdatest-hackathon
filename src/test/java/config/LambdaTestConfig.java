f0e191f8a14e77ed10e44e3f48b3a7aa336805c2
Kunal Sharma <kunal.ucet@gmail.com>
2025-04-12 21:21:31 +0530

    updated lamdatest config

--- src/test/java/config/LambdaTestConfig.java
+++ src/test/java/config/LambdaTestConfig.java
@@ -3,10 +3,13 @@ package config;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class LambdaTestConfig {
-    public static final String LT_USERNAME = System.getenv("LT_USERNAME");
-    public static final String LT_ACCESS_KEY = System.getenv("LT_ACCESS_KEY");
-
-    public static final String GRID_URL = "https://" + LT_USERNAME + ":" + LT_ACCESS_KEY + "@hub.lambdatest.com/wd/hub";
+	
+    public static final String LT_USERNAME = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
+    public static final String LT_ACCESS_KEY = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
+    
+    static String hub = "@hub.lambdatest.com/wd/hub";
+    
+    public static final String GRID_URL = "https://" + LT_USERNAME + ":" + LT_ACCESS_KEY + hub;
 
     public static DesiredCapabilities getCapabilities(String browser, String version, String platform) {
         DesiredCapabilities caps = new DesiredCapabilities();
